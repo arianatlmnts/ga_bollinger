6cd439b973c230cb9fbf1a806d5a5ca5659199f0
israelcicese <57415041+israelcicese@users.noreply.github.com>
2020-03-18 15:21:44 -0700

    implemantacion del genotipo en main

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import random 
+
+'''  sección del geneotipo'''
+
+class Candidate(object):
+    def __init__(self, genotype, fitness):
+        self.genotype = genotype
+        self.fitness = fitness
+
+    def mutation(self): #cambiar metodo esta mutacion no es aplicable a este metodo
+        r = random.randint(0,100)
+        p_mut = 10
+        if r <= p_mut:
+            x,y = random.sample(range(1, len(self.genotype)), 2)
+            self.genotype[x],self.genotype[y] = self.genotype[y],self.genotype[x]
+        self.fitness = fitness(self.genotype)
+
+
+
+
+
 
 '''  ## Funcion duplicada
 def returns_fitness(returns):
@@ -59,7 +80,7 @@ def WMA (close,window = 20 ):
 
     return (mean)
 
-def EMA (close,window = 20,alfa = 0.08):
+def EMA (close,window = 20,alfa = 0.06):
     #alfa = 2/(window + 1)
     mean = []
     window_values = []
@@ -77,8 +98,6 @@ def EMA (close,window = 20,alfa = 0.08):
             factor_ventana =np.sum(factor_ventana/sum_exp)
 
             mean.append(factor_ventana)
-
-
 
         if i > window:
             window_values.pop(0)
@@ -131,7 +150,10 @@ def buy_sell(cierre,superior,inferior,window_size):
 
 
 def calculate_bollinger_bands(data,n=20,k1=2,k2=2):
-    ma = data.rolling(window=n).mean()
+    #ma = data.rolling(window=n).mean()
+    #ma = SMA(data, window = n)
+    #ma = WMA(data, window = n)
+    ma = EMA(data, window = n)
     std = data.rolling(window=n).std()
     upper_band = ma + (k1*std)
     lower_band = ma - (k2*std)
@@ -139,7 +161,7 @@ def calculate_bollinger_bands(data,n=20,k1=2,k2=2):
     return ma, upper_band, lower_band
 
 
-if __name__ == '__main__':
+def fitness(genes):
     df = pd.read_csv('data/EURUSD_Candlestick_15_m_BID_01.01.2007-31.12.2007.csv')
 
     #p = 200
@@ -161,6 +183,12 @@ if __name__ == '__main__':
     regreso_po, regreso_neg, usd = buy_sell(cierre=Close, superior=Upper, inferior=Lower, window_size=n)
 
     returns_fit = returns_fitness(regreso_po, regreso_neg)
+
+    return(returns_fit)
+
+
+
+    ''' para hacer el ciclo de procesamiento no es necesario graficar
     print('return fitness: ',returns_fit)
     print('usd: ',usd)
 
@@ -170,3 +198,34 @@ if __name__ == '__main__':
     plt.plot(upper)
     plt.plot(lower)
     plt.show()
+    '''
+
+def main():
+    population_size = 1
+    C = []
+    #p_mut = 0.1 # se utiliza en otro punto afuera de este ciclo
+    g = np.zeros(4)
+    # initialize random population
+    contador_w = 0
+    while  (contador_w <= population_size): 
+
+        x = random.uniform(1,3) #Valor aleatorio para el alpha de la banda superior
+        y = random.uniform(1,3) #Valor aleatorio para la banda inferior
+        g[0], g[1] = x, y
+        g[2] = random.randint(0,2) #Selecciona el tipo de media a usar
+        g[3] = random.randint(20,200) #
+        C.append(Candidate(g.tolist(),fitness(g)))
+        contador_w += 1
+
+    for i in range(len(C)):
+        print (C[i].fitness)
+        print (C[i].genotype)
+
+    # best fitness & survival
+    ##C.sort(key=lambda x: x.fitness, reverse=True) # ordenar por fitness
+    ##C = C[:population_size]
+
+
+
+if __name__ == "__main__":
+    main()
